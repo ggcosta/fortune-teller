39c3a6aac5abae9a5d4e8af52b3ebfe365622928
Gonçalo Costa <goncaloc@live.com>
2024-01-15 17:26:33 +0000

    Added animation to start component and changed header title

--- components/Start.jsx
+++ components/Start.jsx
@@ -1,19 +1,46 @@
-import { useRef } from 'react';
-import Image from "next/image";
+import { useRef, useState, useEffect } from "react";
 
 const Start = ({ handleStart }) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const circleRef = useRef(null);
   const timer = useRef(null);
 
   const handleMouseDown = () => {
-    timer.current = setTimeout(handleStart, 1000);
+    setIsPressed(true);
+    timer.current = setTimeout(handleStart, 2000);
   };
 
   const handleMouseUp = () => {
+    setIsPressed(false);
     clearTimeout(timer.current);
   };
 
+  useEffect(() => {
+    if (isPressed) {
+      const startTime = Date.now();
+      const id = setInterval(() => {
+        const elapsed = Date.now() - startTime;
+        if (elapsed > 2000) {
+          clearInterval(id);
+        } else {
+          circleRef.current.setAttribute("r", (300 * elapsed) / 2000);
+        }
+      }, 20);
+      return () => clearInterval(id);
+    } else {
+      circleRef.current.setAttribute("r", 0);
+    }
+  }, [isPressed]);
+
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
       <button
         style={{
           border: "none",
@@ -29,13 +56,25 @@ const Start = ({ handleStart }) => {
         onMouseUp={handleMouseUp}
         onMouseLeave={handleMouseUp}
       >
-        <Image
-          src="/images/hand_placement.png"
-          alt="Button Image"
-          width={600}
-          height={600}
-          priority
-        />
+        <svg width="600" height="600">
+          <circle
+            cx="300"
+            cy="300"
+            r="300"
+            stroke="black"
+            strokeWidth="3"
+            fill="transparent"
+          />
+          <circle
+            ref={circleRef}
+            cx="300"
+            cy="300"
+            r="0"
+            stroke="green"
+            strokeWidth="3"
+            fill="green"
+          />
+        </svg>
       </button>
       <p>Place your hand in the area above to start</p>
     </div>
